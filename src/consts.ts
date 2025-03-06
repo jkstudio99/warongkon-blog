fe746c785b1e9c21f298f99a9e7355293d6d7525
jkstudio99 <143254106+jkstudio99@users.noreply.github.com>
2025-03-06 20:41:29 +0700

    update tagline

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '"Warongkon"';
-export const SITE_TAGLINE = 'Junior Software Engineer'; // site tagline
+export const SITE_TITLE = 'Warongkon';
+export const SITE_TAGLINE = 'Blog'; // site tagline
 export const SITE_DESCRIPTION = 'A blog by Warongkon'; // site meta description for SEO
